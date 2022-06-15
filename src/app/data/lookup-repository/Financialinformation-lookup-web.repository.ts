5c8c58be8b8f18a5676eea95d743e8c1abaa41b8
edsonrdl <edsonrodrigo11111@gmail.com>
2022-06-15 11:14:28 -0300

    feat: Adicionando lookup-respository a FinancialInformation

---
+++ src/app/data/lookup-repository/Financialinformation-lookup-web.repository.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { asyncScheduler, Observable, scheduled } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { LookupModel } from 'src/app/core/models/lookup.model';
+import { BaseHttpService } from 'src/app/services/http/base-http.service';
+import { environment } from 'src/environments/environment';
+import { FinancialInformationLookupRepository } from '../../core/lookups/ba-usu-lookup/financial-information-lookup.repository';
+import { LookupWebEntity } from './helpers/lookup-web-entity';
+import { LookupWebRepositoryMapper } from './helpers/lookup-web-repository-mapper';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FinancialInformationLookupWebRepository extends FinancialInformationLookupRepository {
+  mapper = new LookupWebRepositoryMapper();
+
+  constructor(public http: BaseHttpService) {
+    super();
+  }
+
+  GetAll(): Observable<LookupModel[]> {
+    return this.http
+      .getAll<LookupWebEntity[]>(`${environment.SYSTEMUSER}common/lookup-financial-informartion `)
+      .pipe(
+        map((item) => {
+          return item.data;
+        })
+      )
+      .pipe(
+        map((item: any) => {
+          return this.mapper.mapFromList(item.data);
+        })
+      );
+  }
+
+  GetAllByName(nome: string): Observable<LookupModel[]> {
+    return this.http
+      .getAll<LookupWebEntity[]>(
+        `${environment.SYSTEMUSER}common/lookup-predicate-financial-informartion -by-name/${nome}`
+      )
+      .pipe(
+        map((item) => {
+          return item.data;
+        })
+      )
+      .pipe(
+        map((item: any) => {
+          return this.mapper.mapFromList(item.data);
+        })
+      );
+  }
+}
