46a883eace6111a0193b3f4d8561edef67b7cd86
edsonrdl <edsonrodrigo11111@gmail.com>
2022-06-29 15:04:27 -0300

    feat: Adicionado ListFinancialInformationModel ao repositories

--- src/app/core/repositories/financial-information.repository.ts
+++ src/app/core/repositories/financial-information.repository.ts
@@ -1,13 +1,25 @@
 import { Observable } from 'rxjs';
-import { FinancialInformationModel } from '../models/financial-information.model';
+import {
+  FinancialInformationModel,
+  ListFinancialInformationModel,
+} from '../models/financial-information.model';
 import { PageFilterModel } from '../utils/filters/page-filter.model';
 import { PageResultModel } from '../utils/responses/page-result.model';
 import { SingleResultModel } from '../utils/responses/single-result.model';
 
 export abstract class FinancialInformationRepository {
-  abstract getFinancialInformationById(id: number): Observable<SingleResultModel<FinancialInformationModel>>;
-  abstract getAllFinancialInformation(filter: PageFilterModel): Observable<PageResultModel<FinancialInformationModel>>;
-  abstract postFinancialInformation(param: FinancialInformationModel): Observable<FinancialInformationModel>;
+  abstract postListFinancialInformation(
+    param: ListFinancialInformationModel[]
+  ): Observable<FinancialInformationModel[]>;
+  abstract getFinancialInformationById(
+    id: string
+  ): Observable<SingleResultModel<FinancialInformationModel>>;
+  abstract getAllFinancialInformation(
+    filter: PageFilterModel
+  ): Observable<PageResultModel<FinancialInformationModel>>;
+  abstract postFinancialInformation(
+    param: FinancialInformationModel
+  ): Observable<FinancialInformationModel>;
   abstract putFinancialInformation(param: FinancialInformationModel): Observable<void>;
-  abstract deleteFinancialInformation(id: number): Observable<void>;
+  abstract deleteFinancialInformation(id: string): Observable<void>;
 }
