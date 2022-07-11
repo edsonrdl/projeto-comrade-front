85fdfd77a56c2990e13c6e75cb97f7180702c3ef
edsonrdl <edsonrodrigo11111@gmail.com>
2022-07-11 16:44:46 -0300

    feat:Alterado versão das entidades

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,6 @@ export const environment = {
   traceRequest: false,
   googleMapsApiKey: '',
   TOKEN: 'https://localhost:44304/api/v1/',
-  SYSTEMUSER: 'https://localhost:44304/api/v1/',
+  SYSTEMUSER: 'https://localhost:44304/api/v2/',
+  FINANCIALINFORMATION: 'https://localhost:44304/api/v2/',
 };
