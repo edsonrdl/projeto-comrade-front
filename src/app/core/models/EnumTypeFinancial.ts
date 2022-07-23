579dd61040e96a4fd524c6b26e1e7403cad2d911
edsonrdl <edsonrodrigo11111@gmail.com>
2022-07-22 23:38:49 -0300

    feat: Criado EnumTypeFinancial

---
+++ src/app/core/models/EnumTypeFinancial.ts
@@ -0,0 +1,12 @@
+export enum EnumTypeFinancial {
+  None,
+  Debit = 1,
+  Ticket = 2,
+  Financing = 3,
+  Credit = 4,
+  LoanReceipt = 5,
+  Sales = 6,
+  TedReceipt = 7,
+  ReceiptDoc = 8,
+  Rent = 9,
+}
