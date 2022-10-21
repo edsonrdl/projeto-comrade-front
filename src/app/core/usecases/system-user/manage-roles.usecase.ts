a72e68da7c68f952ff1c597528fce95ff7403b2f
edsonrdl <edsonrodrigo11111@gmail.com>
2022-10-21 14:54:53 -0300

    feat: Correções class role

--- src/app/core/usecases/system-user/manage-roles.usecase.ts
+++ src/app/core/usecases/system-user/manage-roles.usecase.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { UseCase } from '../../utils/bases/use-case';
 import { Observable } from 'rxjs';
 import { SystemUserRepository } from '../../repositories/system-user.repository';
-import { SystemUserManagePermissionsModel } from '../../models/system-user-manage-permissions.model';
 import { SystemUserManageRolesModel } from '../../models/system-user-manage-roles.model';
 
 @Injectable({
